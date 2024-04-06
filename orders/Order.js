a573388d48948c620c82731fc2c57c2b6e7714cd
Hajar Chakloul <hajarchakloul@gmail.com>
2024-04-06 14:52:28 +0000

    commit  in Order.js

---
+++ orders/Order.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const orderSchema = mongoose.Schema({
+customerID: {
+type: mongoose.SchemaTypes.ObjectId,
+require: true
+},
+bookID: {
+type: mongoose.SchemaTypes.ObjectId,
+require: true
+},
+initialDate: {
+type: Date,
+require: true
+},
+deliveryDate: {
+type: Date,
+require: false
+}
+})
