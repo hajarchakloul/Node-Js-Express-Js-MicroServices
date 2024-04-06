1826f089948ccf439da8c52de56703f3863aa396
Hajar Chakloul <hajarchakloul@gmail.com>
2024-04-06 14:11:18 +0000

    commit add books.js

---
+++ books/books.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require('express');
+
+// Connect
+require('../db/db');
+
+const Book = require('./Book');
+
+const app = express();
+const port = 3000;
+app.use(express.json())
+
+app.post('/book', (req, res) => {
+const newBook = new Book({...req.body});
+newBook.save().then(() => {
+res.send('New Book added successfully!')
+}).catch((err) => {
+res.status(500).send('Internal Server Error!');
+
+})
+})
